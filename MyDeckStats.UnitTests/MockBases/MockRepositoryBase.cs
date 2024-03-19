f34379fac1bac808e06a64f34e8b0d5e69b4b175
Fletcher Trueblood <ftrueblood@jgboswell.com>
2024-03-19 15:50:18 -0700

    Add Testing Project
    Add testing project, base mock repo, and MtgCardService tests

---
+++ MyDeckStats.UnitTests/MockBases/MockRepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using MyDeckStats.Domain.Entities;
+using MyDeckStats.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDeckStats.UnitTests.MockBases
+{
+    public static class MockRepositoryBase
+    {
+        public static Mock<TRepo> MockRepo<TRepo, T>(ICollection<T> list)
+           where TRepo : class, IRepositoryBase<T>
+           where T : EntityBase
+        {
+            var mock = new Mock<TRepo>();
+
+            mock.Setup(x => x.Add(It.IsAny<T>())).Returns((T x) =>
+            {
+                list.Add(x);
+                return x;
+            });
+
+            mock.Setup(x => x.GetById(It.IsAny<Guid>())).Returns((Guid x) => { return list.FirstOrDefault(y => y.Id == x); });
+
+            mock.Setup(x => x.GetAll()).Returns(() => list);
+
+            mock.Setup(x => x.Update(It.IsAny<T>())).Returns((T Entity) => { return Entity; });
+
+            mock.Setup(x => x.Delete(It.IsAny<T>())).Returns((T Entity) => 
+            {
+                list.Remove(Entity);
+                return !list.Contains(Entity); 
+            });
+
+            mock.Setup(x => x.Filter(It.IsAny<Func<T, Boolean>>())).Returns((Func<T, bool> x) => { return list.Where(x); });
+
+
+            return mock;
+        }
+    }
+}
