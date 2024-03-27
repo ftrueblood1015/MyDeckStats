2dc2ecfd0ce50f5f4e2b22b0977c8082f3dbfda6
Fletcher Trueblood <ftrueblood@jgboswell.com>
2024-03-27 14:31:50 -0700

    Add Viewable Processed Data
    Add processed card data to card detail

--- MyDeckStats/Pages/Mtg/Cards/MtgCardDetail.razor.cs
+++ MyDeckStats/Pages/Mtg/Cards/MtgCardDetail.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MyDeckStats.Domain.Entities.Mtg.Cards;
+using MyDeckStats.Domain.Interfaces.Services.Mtg;
 using MyDeckStats.Pages.Shared;
 
 namespace MyDeckStats.Pages.Mtg.Cards
@@ -9,13 +10,32 @@ namespace MyDeckStats.Pages.Mtg.Cards
         [Parameter]
         public string EntityId { get; set; }
 
+        [Inject]
+        private IMtgKeywordService? MtgKeywordService { get; set; }
+
+        [Inject]
+        private IColorIdentityService? ColorIdentityService { get; set; }
+
+        [Inject]
+        private ICardTypeService? CardTypeService { get; set; }
+
         public MtgCard? Entity { get; set; } = new MtgCard();
+
+        private List<MtgKeyword>? MtgKeywords { get; set; }
+
+        private List<ColorIdentity>? ColorIdentities { get; set; }
+
+        private List<CardType>? CardTypes { get; set; }
 
         private bool IsReadOnly = true;
 
         protected override async Task OnInitializedAsync()
         {
             Entity = await SetEntity();
+            await GetCardKeywords();
+            await GetColorIdentities();
+            await GetCardTypes();
+            await base.OnInitializedAsync();
         }
 
         public async Task<MtgCard> SetEntity()
@@ -28,6 +48,39 @@ namespace MyDeckStats.Pages.Mtg.Cards
             {
                 return await base.GetEntity(new Guid(EntityId));
             }
+        }
+
+        private async Task GetCardKeywords()
+        {
+            if (MtgKeywordService == null)
+            {
+                throw new Exception($"{nameof(MtgKeywordService)}  is null!");
+            }
+
+            var Response = MtgKeywordService.Filter(x => x.MtgCardId == new Guid(EntityId));
+            MtgKeywords = Response != null ? Response.ToList() : new List<MtgKeyword>();
+        }
+
+        private async Task GetColorIdentities()
+        {
+            if (ColorIdentityService == null)
+            {
+                throw new Exception($"{nameof(ColorIdentityService)}  is null!");
+            }
+
+            var Response = ColorIdentityService.Filter(x => x.MtgCardId == new Guid(EntityId));
+            ColorIdentities = Response != null ? Response.ToList() : new List<ColorIdentity>();
+        }
+
+        private async Task GetCardTypes()
+        {
+            if (CardTypeService == null)
+            {
+                throw new Exception($"{nameof(CardTypeService)}  is null!");
+            }
+
+            var Response = CardTypeService.Filter(x => x.MtgCardId == new Guid(EntityId));
+            CardTypes = Response != null ? Response.ToList() : new List<CardType>();
         }
     }
 }
