e2f5988443f7e8771f285ddfdbd54e0b45fd6b2b
Fletcher Trueblood <ftrueblood@jgboswell.com>
2024-03-28 13:40:56 -0700

    Front End Master Purposes
    Add pages for master purpose summary and detail

---
+++ MyDeckStats/Pages/Mtg/Purposes/MasterPurposeDetail.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.IdentityModel.Tokens;
+using MyDeckStats.Domain.Entities.Mtg.Cards;
+using MyDeckStats.Pages.Shared;
+
+namespace MyDeckStats.Pages.Mtg.Purposes
+{
+    public partial class MasterPurposeDetail : DetailPageBase<MasterPurpose>
+    {
+        [Parameter]
+        public string EntityId { get; set; }
+
+        public MasterPurpose? Entity { get; set; } = new MasterPurpose();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Entity = await SetEntity();
+        }
+
+        public async Task<MasterPurpose> SetEntity()
+        {
+            if (EntityId.IsNullOrEmpty())
+            {
+                return new MasterPurpose();
+            }
+            else
+            {
+                return await base.GetEntity(new Guid(EntityId));
+            }
+        }
+    }
+}
---
+++ MyDeckStats/Pages/Mtg/Purposes/MasterPurposeSummary.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyDeckStats.Commands.Navigation;
+using MyDeckStats.Domain.Entities.Mtg.Cards;
+using MyDeckStats.Pages.Shared;
+
+namespace MyDeckStats.Pages.Mtg.Purposes
+{
+    public partial class MasterPurposeSummary : SummaryPageBase<MasterPurpose>
+    {
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+
+        private string DetailRoute = "masterpurpose";
+
+        public override Func<MasterPurpose, bool> _quickFilter => x =>
+        {
+            if (string.IsNullOrWhiteSpace(_searchString))
+                return true;
+
+            if (x.Name!.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            return false;
+        };
+
+        public void View(MasterPurpose purpose, string route)
+        {
+            var navCommand = new NavigationCommand(NavigationManager!, $"/{route}/{purpose.Id}", false);
+            navCommand.Execute();
+        }
+    }
+}
